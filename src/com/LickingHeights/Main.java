51fd555929b46579e54754fe29fc71bca0dfc31c
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-05-07 08:03:21 -0400

    print name

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,13 @@
 package com.LickingHeights;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String name;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is your name?");
+        name = keyboard.nextLine();
+        System.out.println(name);
     }
+
 }
