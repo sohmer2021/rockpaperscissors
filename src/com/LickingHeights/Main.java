b4d253ab0b797d7182caaedbb874e8e00fce517b
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-05-13 08:11:29 -0400

    variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,13 +1,54 @@
 package com.LickingHeights;
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        String name;
+        Random random = new Random();
         Scanner keyboard = new Scanner(System.in);
+        String name;
         System.out.println("What is your name?");
         name = keyboard.nextLine();
         System.out.println(name);
-    }
+        int input;
+        int compInput;
+        System.out.println("Pick 0 (Rock), 1 (Paper), or 2 (Scissors)");
+        input = keyboard.nextInt();
+        {
 
-}
+        }
+        if (input == 0) ;
+        System.out.println("Rock");
+
+        {if (input == 1) ;
+            System.out.println("Paper");
+
+            { if (input == 2) ;
+                System.out.println("Scissors");
+
+                {
+                    if (compInput == 1)
+
+                    System.out.println("You chose rock, computer chose paper");
+            System.out.println("You win");
+
+        }
+        {
+            if (compInput == 2);
+
+            System.out.println("You chose rock, computer chose scissors");
+            System.out.println("You win");
+
+            {
+                if (compInput == 3);
+
+                System.out.println("You chose paper, computer chose rock");
+                System.out.println("You win");
+
+
+}}}}}}
+
+
+
+
+
