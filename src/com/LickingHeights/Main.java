aa3a3a140a63f9360c3a5b03f7109c1bf49dc6d8
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-05-16 08:10:36 -0400

    if statements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         Random random = new Random();
+        int compPick = (int)(Math.random()*100);
         Scanner keyboard = new Scanner(System.in);
         String name;
         System.out.println("What is your name?");
@@ -16,55 +17,61 @@ public class Main {
         input = keyboard.nextInt();
         int userScore;
         int compScore;
-        {
+        boolean loop = true;
+        while (loop){
 
-        }
+
+
+
         if (input == 0) ;
         System.out.println("Rock");
 
-        {
-            if (input == 1) ;
-            System.out.println("Paper");
-
             {
-                if (input == 2) ;
-                System.out.println("Scissors");
+                if (input == 1) ;
+                System.out.println("Paper");
 
+                {
+                    if (input == 2) ;
+                    System.out.println("Scissors");
+
+                    if (compInput == 1 && input == 0) ;
+
+                    System.out.println("You chose rock, computer chose paper ");
+                    System.out.println("You lose");
+                    break;
+
+
+                }
+                if (compInput == 2 && input == 0) ;
+                System.out.println("You chose rock, computer chose scissors");
+                System.out.println("You win");
+                break;
             }
-            if (compInput == 1 && input == 0) ;
-
-            System.out.println("You chose rock, computer chose paper ");
-            System.out.println("You lose");
-
-
-        }
-         if (compInput == 2 && input == 0) ;
-        System.out.println("You chose rock, computer chose scissors");
-        System.out.println("You win");
-
-
-        {
-            if (compInput == 2 && input == 1);
+                if (compInput == 2 && input == 1);
 
             System.out.println("You chose paper, computer chose scissors");
             System.out.println("You lose");
+            break;
 
 
-            {  if (compInput == 0 && input == 1);
+            {   if (compInput == 0 && input == 1);
 
             System.out.println("You chose paper, computer chose rock");
             System.out.println("You win");
+            break;
 
             {
-                if (compInput == 1 && input == 2);
+                 if (compInput == 1 && input == 2);
 
                 System.out.println("You chose scissors, computer chose paper");
                 System.out.println("You win");
+                break;
 
-                {  if (compInput == 0 && input == 2);
+                {   if (compInput == 0 && input == 2);
 
                 System.out.println("You chose scissors, computer chose rock");
                 System.out.println("You lose");
+                break;
 
 
 }}}}}}
